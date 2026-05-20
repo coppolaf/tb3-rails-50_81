32a5642e19b65ef5a78526956ab3cb651a5a40f6
Francesco <f.coppola.vr@gmail.com>
2026-05-20 18:52:25 +0200

    add rails 7.2 sprockets dummy app deliverable

---
+++ deliverables/rails_7_2_sprockets_dummy/config/application.rb
@@ -0,0 +1,18 @@
+require "rails"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
+require "twitter-bootstrap-rails"
+
+Bundler.require(*Rails.groups)
+
+module AuditDummy
+  class Application < Rails::Application
+    config.load_defaults 7.2
+    config.eager_load = false
+    config.secret_key_base = "audit-secret-key-base"
+    config.assets.compile = true if config.respond_to?(:assets)
+    config.public_file_server.enabled = true if config.respond_to?(:public_file_server)
+  end
+end
